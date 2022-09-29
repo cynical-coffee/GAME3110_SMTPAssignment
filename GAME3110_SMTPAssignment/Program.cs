a2f1cf978d26f4e1f8b2116a8abee15a2bbd2bf5
Cynical-Coffee <mparis003@gmail.com>
2022-09-29 13:19:33 -0400

    Created VS project.

---
+++ GAME3110_SMTPAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GAME3110_SMTPAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
